ddc06e77b38842c9c1adb2636654174dac55ee56
Ostrix84 <131650955+Ostrix84@users.noreply.github.com>
2024-06-27 09:59:38 +0200

    Delete s.sh

--- s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Démarrage du script"
-wget -q https://repos.influxdata.com/influxdb.key
-echo '23a1c8836f0afc5ed24e0486339d7cc8f6790b83886c4c96995b88a061c5bb5d influxdb.key' | sha256sum -c && cat influxdb.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
-sudo apt-get update
-sudo apt install influxdb2
-sudo systemctl start influxdb
-echo "Mise à jour terminée"
